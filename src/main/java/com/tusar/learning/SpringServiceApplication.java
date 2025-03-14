69b64f0e8a5cfee55eb1a430e51aa19c5b97a04d
farruck.tusar <farruck.tusar@solactive.com>
2025-03-14 10:12:00 +0100

    Initial commit

---
+++ src/main/java/com/tusar/learning/SpringServiceApplication.java
@@ -0,0 +1,15 @@
+package com.tusar.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceApplication
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(SpringServiceApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/tusar/learning/SpringServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tusar.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceApplicationTests
+{
+
+   @Test
+   void contextLoads()
+   {
+   }
+
+}
